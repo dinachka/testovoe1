623be4ec00062a11f67000abc698e3e7fbe4ea05
dina <dinachernykh1@gmail.com>
2023-06-27 17:47:57 +0300

    Initial Commit

---
+++ testovoe1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testovoe1
+//
+//  Created by Дина Черных on 27.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
